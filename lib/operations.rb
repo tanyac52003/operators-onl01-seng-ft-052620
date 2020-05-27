34e34eba6ba7024e3dbbcb68e414f2a1c1763404
Tanya Hodges <tanyac52003@gmail.com>
2020-05-27 01:54:00 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
+  if speed > 60 || speed < 40
+    true 
+  else 
+    false 
+  end 
+end 
 
-end
+ 
 
 
 
 def not_safe?(speed)
+  speed > 60 || speed < 40 ? true : false 
+end 
 	
-end
 	
 
 
